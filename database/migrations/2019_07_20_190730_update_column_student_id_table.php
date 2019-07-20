81a5ab8167c5033828a75aae33849a25deffe511
duonghvph05531 <duonghvph05531@fpt,edu,vn>
2019-07-21 03:16:42 +0700

    lab3 - again

---
+++ database/migrations/2019_07_20_190730_update_column_student_id_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateColumnStudentIdTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('students',function(Blueprint $table){
+
+            $table->bigIncrements('id')->change();
+            $table->integer('class_id');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('students',function(){
+            $table->dropColumn('id');
+            $table->bigInteger('class_id')->change();
+        });
+    }
+}
